9788163ff70c07fee71a5622322cecd2f30f42d6
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-03-11 11:38:57 +0530

    advance py day1 commit 1.1

--- Topic/AdvDay1.py
+++ Topic/AdvDay1.py
@@ -8,3 +8,18 @@ def sum(a , b , d = 40):            #def key workd function name sum : thats all
                              # (d = 40) syntax of default parameter
 sum(2,5)                          # 2 and 5 are  argument 
 sum(10,30,60)               # argument 60 overlap parameter d = 40 to d = 60
+sum(d=30 , a=50, b=20)          #this called keyword argument
+
+def evenodd(a):
+    if a%2 == 0:
+        print("even")
+    else:
+        print("odd")    
+
+evenodd(12)
+evenodd(13)
+evenodd(14)
+evenodd(15)
+evenodd(16)
+evenodd(17)
+evenodd(18)
