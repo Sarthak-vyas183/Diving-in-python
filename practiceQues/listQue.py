ffcae812268ebc6839d54282520f8bf34665dfe8
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-03-19 21:25:14 +0530

    listQues

---
+++ practiceQues/listQue.py
@@ -0,0 +1,78 @@
+# Que : 1 accept list element and reprint :
+# l = []
+# count = int(input("Enter the number how many element you want to enter : "))
+
+# for i in range(count):
+    # z = int(input("Enter the number"))
+    # l.append(z) 
+
+#print("list of ",count ,"element is : ",l))
+# second way : 
+# l = input("enter the value with proper spaces").split()
+# print(l) 
+
+# Que 2 : print the list element in reverse order : 
+# lists = [2,5,6,9,87,2,3]
+# new = []
+
+# for i in range(len(lists)-1 , -1 , -1):
+#     #    new.append(lists[i])
+
+# # print(new)
+
+
+
+
+
+# que : print positive and negative element of a list in a separate list : 
+
+# L = [1,-9,5,-2,5,6,-7,-30,-99,256]
+# pos = []
+# neg = []
+# for i in range(0,len(L)-1):
+#     if(L[i] > 0 ) :
+#         pos.append(L[i])
+#     else:
+#         neg.append(L[i])  
+
+# print(f"your positive element are {pos} \n Negative elements are {neg}")
+
+
+
+
+
+# Que : Find the greatest element and print its index too 
+li = [2,96,69,77,145,20]
+# max = 0
+# index = 0 
+
+# for i in range(len(li)):
+#     if li[i] > max:
+#         max = li[i] 
+#         index = i 
+# print(max , "index" , index)
+#  or 
+# print(max(2,99))   this is the function which give you max number . 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
